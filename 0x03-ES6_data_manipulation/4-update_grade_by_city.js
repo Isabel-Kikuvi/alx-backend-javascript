15c6e64ea2a0bbcd9b418a9dbdddbbdf731506a7
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-12-08 11:28:13 -0800

    minor edits

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,22 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-prototype-builtins */
+export default function updateStudentGradeByCity(list, city, newGrade) {
+  return list
+    .filter((obj) => obj.location === city)
+    .map((student) => {
+    //   console.log(newGrade);
+      newGrade.map((studentGrade) => {
+        if (studentGrade.studentId === student.id) {
+          // eslint-disable-next-line no-param-reassign
+          student.grade = studentGrade.grade;
+        }
 
-      if (gradeObj) {
-        return {
-          ...student,
-          grade: gradeObj.grade,
-        };
-      } else {
-        return {
-          ...student,
-          grade: 'N/A',
-        };
-      }
+        if (!student.hasOwnProperty('grade')) {
+          student.grade = 'N/A';
+        }
+        return student;
+      });
+
+      return student;
     });
 }
