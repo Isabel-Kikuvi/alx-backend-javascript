84bd1cec5171ed5add07260537979be5eecbcab3
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2024-01-30 07:53:51 -0800

    1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  if (name.trim().length > 0) {
+    console.log(`Your name is: ${name.trim()}`);
+  }
+  console.log('This important software is now closing');
+  rl.close();
+});
