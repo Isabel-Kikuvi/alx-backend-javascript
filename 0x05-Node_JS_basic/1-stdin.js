7bbbcadcca8e554f15e63d8c037a447676426b07
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2024-01-30 10:12:11 -0800

    minor edits

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,16 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  if (name.trim().length > 0) {
-    console.log(`Your name is: ${name.trim()}`);
-  }
-  console.log('This important software is now closing');
-  rl.close();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
